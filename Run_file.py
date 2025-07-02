66b384364073c5a7f8aac0efde2f5bfd6c96a963
Pranjal Pandey <pythonofapollo0609@gmail.com>
2025-07-02 13:05:10 +0530

    Run_file.py

--- Run_file.py
+++ Run_file.py
@@ -7,16 +7,7 @@ command=input(" (view/add/remove/tag): ").strip().lower()
 if command == "view":
     tp.view_tasks()
 elif command == "add":
-    enter_tasks=input("Enter your tasks:")#use a comma to split the tasks
-    task=enter_tasks.split(",")
-    for t in task:
-        t=t.strip()
-        if t:
-            tp.add_task(t)
-            print("Task added successfully.")
-        else:
-            hp.slow_print("Empty task not added.")
-    
+    tp.add_task()
 elif command == "remove":
     task_id_to_remove = input("Enter the task ID to remove: ").strip()
     tp.remove_task(task_id_to_remove)
@@ -24,4 +15,3 @@ elif command == "tag":
     tp.tag_all_pending_tasks()
 else:
     hp.slow_print("Invalid command. Please enter 'view', 'remove', or 'tag'.")
-
